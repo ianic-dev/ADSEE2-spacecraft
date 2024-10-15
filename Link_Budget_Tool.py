3f6279724389ad60d23883db1d019e4b2671fc64
Bart Andriessen <bmpm.andriessen@gmail.com>
2024-10-15 11:51:45 +0200

    Link_Budget_Tool.py Add formulas

--- Link_Budget_Tool.py
+++ Link_Budget_Tool.py
@@ -7,4 +7,4 @@ c = 3*10**8 #m/s
 Lambda = c / Spacecraft.freq_downlink
 radiation_efficiency =
 anntenna_gain = radiation_efficiency * ((4 * np.pi * (np.pi * Spacecraft.antenna_d**2 / 4))/Lambda**2)
-EIRP = Spacecraft.transmit_P * Spacecraft.transmit_loss_f *
+EIRP = Spacecraft.transmit_P * Spacecraft.transmit_loss_f * f
