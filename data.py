158c7ab02eaaf55f9c5138a417e717261f0cbb63
Ian <ian@ianic.dev>
2024-10-15 10:43:10 +0200

    data fields added

--- data.py
+++ data.py
@@ -1,1 +1,30 @@
+class SC:
+    transmit_P = 0  # W
+    transmit_loss_f = 0  # -
+    freq_downlink = 0  # Hz
+    turnaround_r = 0  # -
+    antenna_d = 0  # m
 
+
+class PL:
+    width_angle = 0  # rad
+    px_size = 0  # rad
+    bit_depth = 0  # bit/px
+    duty_cycle = 0  # -
+    downlink_fraction = 0  # -
+
+
+class Req:
+    uplink_data_rate = 0  # bit/s
+    BER = 0  # -
+
+
+class Orbit:
+    altitude = 0  # m
+    elongation_angle = 0  # rad
+    pointing_offst = 0  # rad
+
+
+class GroundSt:
+    transmit_P = 0  # W
+    antenna_d = 0  # m
