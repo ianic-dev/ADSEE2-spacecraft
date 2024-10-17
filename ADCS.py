d1017e87df434975e063656c3a0d6d55527189a3
Ian <ian@ianic.dev>
2024-10-17 13:48:25 +0200

    x

--- ADCS.py
+++ ADCS.py
@@ -1,6 +1,9 @@
 from case4 import ADCS, Orbit
 import math as m
 
-req_torque = ADCS.mmoi*ADCS.manoevre_angle*4/(ADCS.manoevre_time)
+req_torque = ADCS.mmoi*ADCS.manoevre_angle*4/(ADCS.manoevre_time**2)
 req_momentum = (m.sqrt(2)/2) * ADCS.dist_torque * Orbit.period/4
 print(req_torque)
+print(req_torque, "is the required torque")
+req_thrust = req_torque*0.5/ADCS.moment_arm
+print(req_thrust, "is the required thrust")
