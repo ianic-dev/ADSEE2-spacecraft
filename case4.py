d337697ffe7f9ad6afc226dba50abb9869940925
Ian <ian@ianic.dev>
2024-10-17 10:49:01 +0200

    data added

--- case4.py
+++ case4.py
@@ -2,54 +2,59 @@ import math as m
 
 
 class Spacecraft:
-    transmit_P = 200  # W
+    transmit_P = 1000  # W
     transmit_loss_f = 0.8  # -
-    freq_downlink = 2.2  # GHz
+    freq_downlink = 8.4  # GHz
     freq_downlink = freq_downlink/1e9  # to Hz
-    turnaround_r = 221/240  # -
+    turnaround_r = 749/880  # -
     freq_uplink = freq_downlink/turnaround_r  # Hz
     antenna_d = 1  # m
 
 
 class Payload:
-    width_angle = 0  # deg
+    width_angle = 10  # deg
     width_angle = m.radians(width_angle)  # rad
-    px_size = 0  # arcmin
+    px_size = 0.05  # arcmin
     px_size = px_size*60  # deg
     px_size = m.radians(px_size)  # rad
-    bit_depth = 0  # bit/px
-    duty_cycle = 0  # -
-    downlink_time_p_day = 0  # hr
+    bit_depth = 8  # bit/px
+    duty_cycle = 0.4  # -
+    downlink_time_p_day = 18  # hr
     downlink_fraction = downlink_time_p_day/24  # -
 
 
 class Requirement:
-    uplink_data_rate = 0  # bit/s
-    BER = 0  # -
+    uplink_data_rate = 1e5  # bit/s
+    BER = 1e-6  # -
 
 
 class Orbit:
-    altitude = 0  # km
+    altitude = 500  # km
     altitude = altitude * 1000  # m
-    radius = altitude + 6371000  # m
-    elongation_angle = 0  # deg
+    elongation_angle = 10  # deg
     elongation_angle = m.radians(elongation_angle)  # rad
-    pointing_offst = 0  # deg
+    pointing_offst = 0.05  # deg
     pointing_offst = m.radians(pointing_offst)  # rad
+
+    parent = "Mercury"
+    radius = 2440500 + altitude  # m
+    grav_param = 0.022032e15  # m^3/s^2
+    period = 2*m.pi*m.sqrt(radius**3/grav_param)  # s
 
 
 class Ground_station:
-    transmit_P = 0  # W
-    antenna_d = 0  # m
+    transmit_P = 1000  # W
+    antenna_d = 35  # m
+    loss_factor = 0.7
 
 
 class ACDS:
-    mmoi = 0  # kgm^2
-    dist_torque = 0  # Nm
-    moment_arm = 0  # m
-    manoevre_angle = 0  # deg
-    manoevre_time = 0  # s
+    mmoi = 850  # kgm^2
+    dist_torque = 1e-3  # Nm
+    moment_arm = 1  # m
+    manoevre_angle = 90  # deg
+    manoevre_time = 30  # s
     manoevre_angle = m.radians(manoevre_angle)  # rad
-    pointing_knowledge = 0  # arcsec
+    pointing_knowledge = 5  # arcsec
     pointing_knowledge = pointing_knowledge/3600  # deg
     pointing_knowledge = m.radians(pointing_knowledge)  # rad
