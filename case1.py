c3a165bd25529452e18bb4e7cc2cb60944c0a0ed
Ian <ian@ianic.dev>
2024-10-17 10:27:01 +0200

    added acds data

--- case1.py
+++ case1.py
@@ -12,44 +12,49 @@ class Spacecraft:
 
 
 class Payload:
-    width_angle = 0  # deg
+    width_angle = 20  # deg
     width_angle = m.radians(width_angle)  # rad
-    px_size = 0  # arcmin
+    px_size = 0.1  # arcmin
     px_size = px_size*60  # deg
     px_size = m.radians(px_size)  # rad
-    bit_depth = 0  # bit/px
-    duty_cycle = 0  # -
-    downlink_time_p_day = 0  # hr
+    bit_depth = 8  # bit/px
+    duty_cycle = 0.6  # -
+    downlink_time_p_day = 3  # hr
     downlink_fraction = downlink_time_p_day/24  # -
 
 
 class Requirement:
-    uplink_data_rate = 0  # bit/s
-    BER = 0  # -
+    uplink_data_rate = 1e8  # bit/s
+    BER = 1e-6  # -
 
 
 class Orbit:
-    altitude = 0  # km
+    altitude = 500  # km
     altitude = altitude * 1000  # m
-    radius = altitude + 6371000  # m
-    elongation_angle = 0  # deg
+    elongation_angle = "N/A"  # deg
     elongation_angle = m.radians(elongation_angle)  # rad
-    pointing_offst = 0  # deg
+    pointing_offst = 0.1  # deg
     pointing_offst = m.radians(pointing_offst)  # rad
+
+    parent = "Earth"
+    radius = 6371000 + altitude  # m
+    grav_param = 3.986e14  # m^3/s^2
+    period = 2*m.pi*m.sqrt(radius**3/grav_param)  # s
 
 
 class Ground_station:
-    transmit_P = 0  # W
-    antenna_d = 0  # m
+    transmit_P = 400  # W
+    antenna_d = 0.5  # m
+    loss_factor = 0.7
 
 
 class ACDS:
-    mmoi = 0  # kgm^2
-    dist_torque = 0  # Nm
-    moment_arm = 0  # m
-    manoevre_angle = 0  # deg
-    manoevre_time = 0  # s
+    mmoi = 2.5  # kgm^2
+    dist_torque = 1e-6  # Nm
+    moment_arm = 0.2  # m
+    manoevre_angle = 90  # deg
+    manoevre_time = 10  # s
     manoevre_angle = m.radians(manoevre_angle)  # rad
-    pointing_knowledge = 0  # arcsec
+    pointing_knowledge = 50  # arcsec
     pointing_knowledge = pointing_knowledge/3600  # deg
     pointing_knowledge = m.radians(pointing_knowledge)  # rad
